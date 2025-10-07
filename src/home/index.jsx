786e6881bc6ebdfcb9a8ed10c6f7e76a16532f0b
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-10-07 17:37:04 +0530

    Update index.jsx

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,6 +1,5 @@
 import Header from '@/components/custom/Header'
 import { UserButton } from '@clerk/clerk-react'
-import { AtomIcon, Edit, Share2 } from 'lucide-react'
 import React from 'react'
 
 function Home() {
